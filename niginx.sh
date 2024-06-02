d1db7de9b7e522420aa08e899e4355983e1967b4
iosss <iosss@qq.com>
2024-06-02 13:52:31 +0800

    004

--- niginx.sh
+++ niginx.sh
@@ -8,27 +8,29 @@ NGINX_VERSION=1.24.0
 NGINX_DOWNLOAD_URL=https://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 
 CPUS=`lscpu |awk '/^CPU\(s\)/{print 2}'`
-SRC_DIR=$(pwd)
+SRC_DIR=/00temp/
+rm -rf $SRC_DIR
+mkdir $SRC_DIR
+
 #导入文件中定义的变量 .相当于 source 命令
 . /etc/os-release
+# 安装依赖
+if [ $ID == 'ubuntu' ]; then
+    apt install gcc make libpcre3 libpcre3-dev openssl libssl-dev zlib1g-dev libgd-dev -y
+fi
+if [ $? -ne 0 ]; then
+    echo '安装依赖失败'
+    exit 1
+fi
+
 function install_nginx {
     cd $SRC_DIR
-     rm -rf ${INSTALL_DIR}
     #创建目录
     if [ -e ${NGINX_DIR} ]; then
         echo 'Nginx 已安装'
-        exit 0
-    else
-        mkdir -p ${NGINX_DIR}
-    fi
-    # 安装依赖
-    if [ $ID == 'ubuntu' ]; then
-        apt install gcc make libpcre3 libpcre3-dev openssl libssl-dev zlib1g-dev libgd-dev -y
-    fi
-    if [ $? -ne 0 ]; then
-        echo '安装依赖失败'
-        exit 0
-    fi
+        return 1
+    fi        
+    mkdir -p ${NGINX_DIR}
     # 添加用户
     if ! (id www &> /dev/null);then
         useradd -s /sbin/nologin -r www
@@ -66,10 +68,14 @@ function install_nginx {
     # 检查 Nginx 服务状态
     if systemctl is-active --quiet nginx; then
         echo '安装成功'
+        cd .. && rm nginx* -rf
     else
         echo '安装失败'
         exit 1
     fi
 }
 
+
 install_nginx
+
+rm -rf $SRC_DIR
