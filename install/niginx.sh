ea2953a456abb8b63d1e86850ce40024e6cf9184
test1 <iosss@qq.com>
2024-06-02 05:30:30 +0000

    004

--- install/niginx.sh
+++ install/niginx.sh
@@ -3,8 +3,6 @@ apt update -y
 
 INSTALL_DIR=/lnwp
 NGINX_DIR=$INSTALL_DIR/nginx
-PHP_DIR=$INSTALL_DIR/php
-MYSQL_DIR=$INSTALL_DIR/mysql
 
 NGINX_VERSION=1.24.0
 NGINX_DOWNLOAD_URL=https://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
@@ -13,7 +11,6 @@ CPUS=`lscpu |awk '/^CPU\(s\)/{print 2}'`
 SRC_DIR=$(pwd)
 #导入文件中定义的变量 .相当于 source 命令
 . /etc/os-release
-
 function install_nginx {
     cd $SRC_DIR
      rm -rf ${INSTALL_DIR}
@@ -44,12 +41,35 @@ function install_nginx {
     tar xf nginx-${NGINX_VERSION}.tar.gz
     cd nginx-${NGINX_VERSION}
     #配置参数
-    ./configure --prefix=${NGINX_DIR} --user=www --group=www --with-http_v2_module --with-stream --with-stream_ssl_module --with-stream_ssl_preread_module --with-http_stub_status_module --with-http_ssl_module --with-http_image_filter_module --with-http_gzip_static_module --with-http_gunzip_module --with-http_sub_module --with-http_flv_module --with-http_realip_module --with-http_mp4_module 
+    ./configure --prefix=${NGINX_DIR} --user=www --group=www \
+    --with-pcre \
+    --with-http_v2_module \
+    --with-stream \
+    --with-stream_ssl_module \
+    --with-stream_ssl_preread_module \
+    --with-http_stub_status_module \
+    --with-http_ssl_module \
+    --with-http_image_filter_module \
+    --with-http_gzip_static_module \
+    --with-http_gunzip_module \
+    --with-http_sub_module \
+    --with-http_flv_module \
+    --with-http_realip_module \
+    --with-http_mp4_module 
     #编译安装
     make -j ${CPUS} && make install
     chown -R www:www $NGINX_DIR
     ln -s $NGINX_DIR/sbin/nginx /usr/local/sbin/nginx
-    echo '安装成功'
+    cat ../services/nginx.service | sed "s/\${NGINX_DIR}/${NGINX_DIR}/g" > /etc/systemd/system/nginx.service
+    systemctl daemon-reload
+    systemctl enable --now nginx &> /dev/null
+    # 检查 Nginx 服务状态
+    if systemctl is-active --quiet nginx; then
+        echo '安装成功'
+    else
+        echo '安装失败'
+        exit 1
+    fi
 }
 
 install_nginx
